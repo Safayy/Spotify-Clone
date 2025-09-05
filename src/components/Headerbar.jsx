d540a89cbc4529a68ac4abaa7081ca08dbcf81d2
Safa Yousif <safa.yousif@outlook.com>
2025-09-05 12:21:44 +0800

    Fixed improper brackets

--- src/components/Headerbar.jsx
+++ src/components/Headerbar.jsx
@@ -15,11 +15,11 @@ const Headerbar = () => {
 
   const toggleBrowseActive = () => {
     setIsBrowseActive(!isBrowseActive);
-  
+  };
 
   const toggleNewsActive = () => {
     setIsNewsActive(!isNewsActive);
-  
+  };
 
   return (
     <div className="w-full pt-2 px-2 flex items-center justify-between">
